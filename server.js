c0b1a8746fd726bb9dcd8deb3523fc858212e556
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-08 19:14:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,6 +79,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter' ,function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/:articleName', function(req,res) {
     var articleName=req.params.articleName;
@@ -87,11 +92,7 @@ app.get('/:articleName', function(req,res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-var counter=0;
-app.get('/counter' ,function(res,req){
-    counter=counter+1;
-    res.send(counter.toString());
-});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
