2352a02af986aff3b908f2e468b29ea3190b45c0
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-08 20:41:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,9 +8,9 @@ button.onclick=function(){
       if(request.readystate===XMLHttpRequest.DONE){
           if(request.status===200)
           {
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
+          var counter=request.responseText; 
+        var span=document.getElementById('count');
+        span.innerHTML(counter.toString());
             
           }
       }  
@@ -19,17 +19,29 @@ button.onclick=function(){
     request.open('GET','http://utkarsh1521168.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit-btn');
 submit.onclick=function(){
   
-  var names=['name1','name2','name3'];
-  var list='';
-  for(var i=0;i<names.length;i++)
-  {
-      list+='<li>'+names[i]+'</li>';
-  }
-  var ul=document.getElementById(namelist);
-  ul.innerHTML=list;
+  var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+      
+      if(request.readystate===XMLHttpRequest.DONE){
+          if(request.status===200)
+          {
+            var names=request.responseText;
+            names=(JSON.parse(names));
+            var list='';
+            for(var i=0;i<names.length;i++)
+            list+='<li>'+names[i]+'</li>';
+          
+          var ul=document.getElementById(namelist);
+          ul.innerHTML=list;
+          }
+      }  
+    };
+    var nameInput=document.getElementById('name');
+    var name=nameInput.value;
+    request.open('GET','http://utkarsh1521168.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
 };
