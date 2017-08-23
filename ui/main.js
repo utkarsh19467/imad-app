29b3dcd88730296aaf4b0ea49097362bbeae3dd3
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-24 00:08:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,8 +19,7 @@ button.onclick=function(){
     request.open('GET','http://utkarsh1521168.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
@@ -44,6 +43,8 @@ submit.onclick=function()
           }
       }  
     };
+    var nameInput=document.getElementById('name');
+var name=nameInput.value;
     request.open('GET','http://utkarsh1521168.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 };
