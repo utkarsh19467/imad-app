6cd4b0872656ae5b85fef0e17fbd8d90d5acffdf
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-24 00:46:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool=require('pg').Pool;
+var Pool=require('pg').Pool;
 var config={
     user:'utkarsh1521168',
     database:'utkarsh1521168',
