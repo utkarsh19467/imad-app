1c29dbd16ceb7a87f292f15fbcccfea78deb5c21
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-24 01:01:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,9 +114,27 @@ app.get('/submit-name',function(req,res){
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req,res) {
-    var articleName=req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req,res) {
+  pool.query("SELECT * FROM article-one WHERE title="+req.params.articleName,function(err,result)
+  {
+     if(err)
+     {
+         res.status(500).send(err.toString());
+     }
+     else
+     {
+         if(result.rows.length===0)
+         {
+             res.status(404).send("Article Not Found");
+         }
+         else
+         {
+             var articleData=result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     }
+  });  
+  
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
