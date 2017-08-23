40f0da720ec11b80792d2f2eb45a8489707e655d
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-24 01:03:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/submit-name',function(req,res){
 });
 
 app.get('/articles/:articleName', function(req,res) {
-  pool.query("SELECT * FROM article-one WHERE title="+req.params.articleName,function(err,result)
+  pool.query("SELECT * FROM article-one WHERE title='"+req.params.articleName+"'",function(err,result)
   {
      if(err)
      {
