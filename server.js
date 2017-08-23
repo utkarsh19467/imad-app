ef50a890bf7896f70892b896cf443cbf01675bc2
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-24 00:42:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool=require('pg').Pool;
+var config={
+    user:'utkarsh1521168',
+    database:'utkarsh1521168',
+    host:'db.imad.hasura-app.io',
+   post:'5432',
+   password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles={
@@ -78,7 +85,19 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send(JSON.stringify(result.rows));
+       }
+    });
+});
 var counter=0;
 app.get('/counter' ,function(req,res){
     counter=counter+1;
