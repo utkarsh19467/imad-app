612c9239b813fd99924f08b69736f29e519b3c8e
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-26 18:33:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var bodyParser=require('body-parser');
 var crypto=require('crypto');
 var Pool=require('pg').Pool;
 var config={
@@ -12,6 +13,7 @@ var config={
 };
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 var articles={
  'article-one':{
   title:'article-one |utkarsh shukla',
@@ -96,6 +98,23 @@ app.get('/hash/:input',function(req,res)
 {
    var hashedString=hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
+});
+
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+   var salt=crypto.randomBytes(128).toString('hex');
+   var dbString=hash(password,salt);
+   pool.query('INSERT INTO "download" (username,password) values($1,$2)',[username,dbString],function(err,result){
+        if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           res.send('user successfully created:'+username);
+       }
+   });
 });
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
