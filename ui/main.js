bd5c19df673653c519d4bb24185c6f4f46764908
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-26 19:41:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,52 +1,28 @@
-var button=document.getElementById('counter');
-var counter=0;
-button.onclick=function(){
-    
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-      
-      if(request.readystate===XMLHttpRequest.DONE){
-          if(request.status===200)
-          {
-          var counter=request.responseText; 
-        var span=document.getElementById('count');
-        span.innerHTML(counter.toString());
-            
-          }
-      }  
-    };
-    
-    request.open('GET','http://utkarsh1521168.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
-
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
-     var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-      
-      if(request.readystate===XMLHttpRequest.DONE){
-          if(request.status===200)
-          {
-      var names=request.responseText;
-      names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++)
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
     {
-        list+='<li>'+names[i]+'</li>';
-        
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
-            
-          }
-      }  
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                alert('LOgged In Successfully');
+            }
+            else if(request.status===403)
+            {
+                alert('username/password incorrect');
+            }
+            else if(request.status===500)
+            {
+                alert('Something went wrong');
+            }
+        }
     };
-    var nameInput=document.getElementById('name');
-var name=nameInput.value;
-    request.open('GET','http://utkarsh1521168.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
+  var username=document.getElementById('username').value;
+  var password=document.getElementById('password').value;
+  request.open('POST','http://utkarsh1521168.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type','application/json');
+  request.send(JSON.stringify({username:username,password:password}));
 };
-
- 
