b74c6d51be8f8f8c6ab0018b2522666d1ba4e477
utkarsh19467 <utkarshshukla1996@gmail.com>
2017-08-26 19:04:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,6 +116,37 @@ app.post('/create-user',function(req,res){
        }
    });
 });
+app.post('/login',function(req,res){
+     var username=req.body.username;
+    var password=req.body.password;
+   pool.query('SELECT * FROM "download" WHERE username=$1',[username],function(err,result){
+        if(err)
+       {
+           res.status(500).send(err.toString());
+       }
+       else
+       {
+           if(result.rows.length===0)
+           {
+               res.send(403).send('username/password incorrect');
+           }
+           else
+           {
+               var dbString=result.rows[0].password;
+               var salt=dbString.split('$')[2];
+               var hashedPassword=hash(password,salt);
+               if(hashedPassword===dbString)
+               {
+                   res.send('credentials correct!');
+               }
+               else
+               {
+                   res.send(403).send('username/password incorrect');
+               }
+           }
+       }
+   });
+});
 var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
